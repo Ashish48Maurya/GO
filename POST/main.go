3a43d4d892da716ac0cb0e5d42355ef143ae4a41
Ashish48Maurya <maurya.48.ashish.com>
2024-10-21 00:58:46 +0530

    Day-3: Post req done

--- POST/main.go
+++ POST/main.go
@@ -1,4 +1,43 @@
 package main
-func main(){
-	
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+type Todo struct {
+	UserId    int    `json:"userId`
+	Id        int    `json:"id`
+	Title     string `json:"title`
+	Completed bool   `json:completed`
+}
+
+func main() {
+	todo := Todo{
+		UserId:    123,
+		Title:     "Ashish",
+		Completed: true,
+	}
+
+	res, err := json.Marshal(todo)
+	if err != nil {
+		fmt.Println("Error marshalling:", err)
+		return
+	}
+	jsonString := string(res)
+	jsonReader := strings.NewReader(jsonString)
+	fmt.Println(jsonString, jsonReader)
+	myURL := "https://jsonplaceholder.typicode.com/todos"
+
+	res1, err1 := http.Post(myURL, "application/json", jsonReader)
+	if err1 != nil {
+		fmt.Println("Error sending req:", err1)
+		return
+	}
+	defer res1.Body.Close()
+	data,_ := io.ReadAll(res1.Body)
+	fmt.Println("Data Saved: ",string(data))
 }
